5fdeafe2002343f1102e5b852539f13364c4999a
Matheus Silva <teustenn@gmail.com>
2023-01-16 23:55:20 -0300

    Delete Button and SaveTasks

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,70 @@
-const inpTask = document.querySelector('.inp-task');
-const btnTask = document.querySelector('.btn-task');
-const tasks = document.querySelector('.tasks');
-
-function newTask(txtInput) {
-    const li = document.createElement('li')
-    li.innerHTML = txtInput;
-    tasks.appendChild(li)
-}
+const inpTask = document.querySelector('.inp-task')
+const btnTask = document.querySelector('.btn-task')
+const tasks = document.querySelector('.tasks')
 
 btnTask.addEventListener('click', function() {
-    if (!inpTask.value) return;
-
-    newTask(inpTask.value);
+    if (!inpTask.value) return
+    newTask(inpTask.value)
+    
+    cleanInput()
 })
+
 
 inpTask.addEventListener('keypress', function(e) {
     if (e.keyCode === 13) {
-        if (!inpTask.value) return;
+        if (!inpTask.value) return
         newTask(inpTask.value)
+        
+        cleanInput()
     }
 })
+
+document.addEventListener('click', function(e) {
+    const el = e.target
+
+    if (el.classList.contains('btn-clean')) {
+        el.parentElement.remove()
+    }
+})
+
+
+function newTask(txtInput) {
+    const li = document.createElement('li')
+    
+    li.innerHTML = txtInput
+    tasks.appendChild(li)
+    cleanButton(li)
+}
+
+
+function cleanInput() {
+    inpTask.value = ''
+    inpTask.focus()
+}
+
+
+function cleanButton(li) {
+    const cleanButton = document.createElement('button')
+    
+    cleanButton.innerText = 'Apagar'
+    cleanButton.setAttribute('class', 'btn-clean')
+    cleanButton.setAttribute('title', 'Apagar esta tarefa')
+    li.appendChild(cleanButton)
+}
+
+
+function saveTasks() {
+    const liTasks = tasks.querySelectorAll('li')
+    const listTasks = []
+    
+    
+    for (let task of liTasks) {
+        let taskText = task.innerText
+        
+        taskText = taskText.replace('Apagar', '').trim()
+        listTasks.push(taskText)
+    }
+
+    const tasksJSON = JSON.stringify(listTasks)
+    localStorage.setItem('tasks', tasksJSON)
+}
